45114241c465ad8aa7032dda8a3a0a8ecaab416c
RUPALI1999KUMARI <rupali1999kumari@gmail.com>
2023-02-18 17:36:07 +0530

    API integration done to get newsdata

--- client/src/component/News/News.jsx
+++ client/src/component/News/News.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./News.css";
 
 export const News = (props) => {
+  let news = {
+    source: {
+      id: null,
+      name: "N-tv.de",
+    },
+    author: "n-tv NACHRICHTEN",
+    title:
+      "Nach Widerspruch des Autobauers: Umweltamt kippt Wasser-Auflage für Tesla",
+    description:
+      "Das Landesumweltamt Brandenburg kommt Tesla entgegen: Nach dem Widerspruch des US-Autobauers muss er nun keine Analysen mehr zur Einstufung bestimmter Gemische in Wassergefährdungsklassen vorlegen. Tesla kann dies nun selbstständig tun. Umweltverbände kritisi…",
+    url: "https://www.n-tv.de/wirtschaft/Umweltamt-kippt-Wasser-Auflage-fuer-Tesla-article23927200.html",
+    urlToImage:
+      "https://bilder4.n-tv.de/img/incoming/crop23927203/007132564-cImg_16_9-w1200/393788003.jpg",
+    publishedAt: "2023-02-18T06:36:26Z",
+    content:
+      "Das Landesumweltamt Brandenburg kommt Tesla entgegen: Nach dem Widerspruch des US-Autobauers muss er nun keine Analysen mehr zur Einstufung bestimmter Gemische in Wassergefährdungsklassen vorlegen. T… [+2486 chars]",
+  };
+
+  const [newsData, setNewsData] = useState([news]);
+
   function logout() {
     props.setLoginStatus(false);
     localStorage.clear();
   }
+
+  const getNewsData = async () => {
+    try {
+      const res = await axios.get(
+        "https://newsapi.org/v2/everything?q=tesla&from=2023-01-18&sortBy=publishedAt",
+        {
+          headers: {
+            "x-api-key": "4230c1b8fb3c4337a609cdcaf59501a3",
+          },
+        }
+      );
+      console.log(res.data.articles, "res");
+      let newsArr = res.data.articles;
+      newsArr = newsArr.filter((elem) => elem.urlToImage !== null);
+      setNewsData(newsArr);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getNewsData();
+    console.log(newsData[0], "newsdata");
+  }, []);
 
   return (
     <div className="news">
@@ -27,70 +72,33 @@ export const News = (props) => {
       </div>
 
       <div className="news-box">
-        <div
-          className="news-box-left"
-          style={{ width: "98%", padding: "10px" }}
-        >
+        <div className="news-box-left" style={{ padding: "10px" }}>
           <div className="left-box">
-            <img
-              style={{ width: "100%" }}
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/575957/its-less-risky-to-make-a-small-downpayment-250x141.jpg"
-            />
-            <div class="text">
-              <h2>Before Making A 20% Mortgage Down Payment, Read This</h2>
-              <p>August 8, 2017</p>
-              <p>
-                Making a large downpayment is not the conservative investment
-                you believe it is. The risks of putting 20% down on a home are
-                real. Know what you should do.
-              </p>
+            <img style={{ width: "100%" }} src={newsData[0]?.urlToImage} />
+            <div className="text">
+              <h2>{newsData[0]?.title}</h2>
+              <p>{newsData[0]?.publishedAt}</p>
+              <p>{newsData[0]?.content}</p>
             </div>
           </div>
         </div>
-        <div
-          className="news-box-right"
-          style={{ width: "98%", padding: "10px" }}
-        >
+        <div className="news-box-right" style={{ padding: "10px" }}>
           <div className="news-cover">
-            <article>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/575957/its-less-risky-to-make-a-small-downpayment-250x141.jpg" />
-              <div class="text">
-                <h2>Before Making A 20% Mortgage Down Payment, Read This</h2>
-                <p>August 8, 2017</p>
-                <p>
-                  Making a large downpayment is not the conservative investment
-                  you believe it is. The risks of putting 20% down on a home are
-                  real. Know what you should do.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/575957/minimum-fico-score-drops-to-580-250x141.jpg" />
-              <div class="text">
-                <h2>Minimum FHA Credit Score Requirement Falls 60 Points</h2>
-                <span>August 8, 2017</span>
-                <p>
-                  Credit score requirements are dropping for the FHA mortgage.
-                  Analysis and effect on U.S. home buyers, plus today's live FHA
-                  mortgage rates.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/575957/8.8-1-250x109.png" />
-              <div class="text">
-                <h2>
-                  Mortgage Rates Today, August 8, Plus Lock Recommendations
-                </h2>
-                <span>August 8, 2017</span>
-                <p>
-                  Mortgage rates today are driven by movements in financial
-                  markets worldwide. When the economy heats up, bond price drop,
-                  and rates increase. When the economy pulls back, interest
-                  rates tend to fall.
-                </p>
-              </div>
-            </article>
+            {newsData.slice(1, 4).map((elem, i) => {
+              return (
+                <article key={i}>
+                  <img
+                    style={{ width: "50%", height: "auto" }}
+                    src={elem.urlToImage}
+                  />
+                  <div className="text">
+                    <h2>{elem.title}</h2>
+                    <p>{elem.publishedAt}</p>
+                    <p>{elem.content}</p>
+                  </div>
+                </article>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -98,96 +106,25 @@ export const News = (props) => {
       <div className="bottom-card">
         <div class="main">
           <ul class="cards">
-            <li class="cards_item">
-              <div class="card">
-                <div class="card_image">
-                  <img src="https://picsum.photos/500/300/?image=10" />
-                </div>
-                <div class="card_content">
-                  <h2 class="card_title">Card Grid Layout</h2>
-                  <p class="card_text">
-                    Demo of pixel perfect pure CSS simple responsive card grid
-                    layout
-                  </p>
-                  <button class="btn card_btn">Read More</button>
-                </div>
-              </div>
-            </li>
-            <li class="cards_item">
-              <div class="card">
-                <div class="card_image">
-                  <img src="https://picsum.photos/500/300/?image=5" />
-                </div>
-                <div class="card_content">
-                  <h2 class="card_title">Card Grid Layout</h2>
-                  <p class="card_text">
-                    Demo of pixel perfect pure CSS simple responsive card grid
-                    layout
-                  </p>
-                  <button class="btn card_btn">Read More</button>
-                </div>
-              </div>
-            </li>
-            <li class="cards_item">
-              <div class="card">
-                <div class="card_image">
-                  <img src="https://picsum.photos/500/300/?image=11" />
-                </div>
-                <div class="card_content">
-                  <h2 class="card_title">Card Grid Layout</h2>
-                  <p class="card_text">
-                    Demo of pixel perfect pure CSS simple responsive card grid
-                    layout
-                  </p>
-                  <button class="btn card_btn">Read More</button>
-                </div>
-              </div>
-            </li>
-            <li class="cards_item">
-              <div class="card">
-                <div class="card_image">
-                  <img src="https://picsum.photos/500/300/?image=14" />
-                </div>
-                <div class="card_content">
-                  <h2 class="card_title">Card Grid Layout</h2>
-                  <p class="card_text">
-                    Demo of pixel perfect pure CSS simple responsive card grid
-                    layout
-                  </p>
-                  <button class="btn card_btn">Read More</button>
-                </div>
-              </div>
-            </li>
-            <li class="cards_item">
-              <div class="card">
-                <div class="card_image">
-                  <img src="https://picsum.photos/500/300/?image=17" />
-                </div>
-                <div class="card_content">
-                  <h2 class="card_title">Card Grid Layout</h2>
-                  <p class="card_text">
-                    Demo of pixel perfect pure CSS simple responsive card grid
-                    layout
-                  </p>
-                  <button class="btn card_btn">Read More</button>
-                </div>
-              </div>
-            </li>
-            <li class="cards_item">
-              <div class="card">
-                <div class="card_image">
-                  <img src="https://picsum.photos/500/300/?image=2" />
-                </div>
-                <div class="card_content">
-                  <h2 class="card_title">Card Grid Layout</h2>
-                  <p class="card_text">
-                    Demo of pixel perfect pure CSS simple responsive card grid
-                    layout
-                  </p>
-                  <button class="btn card_btn">Read More</button>
-                </div>
-              </div>
-            </li>
+            {newsData.slice(4).map((elem, i) => {
+              return (
+                <li class="cards_item" key={i}>
+                  <div class="card">
+                    <div class="card_image">
+                      <img
+                        style={{ maxWidth: "100%", height: "auto" }}
+                        src={elem?.urlToImage}
+                      />
+                    </div>
+                    <div class="card_content">
+                      <h2 class="card_title">{elem.title}</h2>
+                      <p class="card_text">{elem.content}</p>
+                      <button class="btn card_btn">Read More</button>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
